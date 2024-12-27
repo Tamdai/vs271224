e3820eefcd57499fc54e37bcfd58049064a424da
Ben_Aphirat <ben.530210155@gmail.com>
2024-12-27 13:20:44 +0700

    first commit

---
+++ python.py
@@ -0,0 +1,41 @@
+# Integer
+integer_example = 10
+
+# Float
+float_example = 10.5
+
+# String
+string_example = "Hello, World!"
+
+# Boolean
+boolean_example = True
+
+# List
+list_example = [1, 2, 3, 4, 5]
+
+# Tuple
+tuple_example = (1, 2, 3, 4, 5)
+
+# Dictionary
+dictionary_example = {"name": "Alice", "age": 25}
+
+# Set
+set_example = {1, 2, 3, 4, 5}
+
+# NoneType
+none_example = None
+
+# Complex
+complex_example = 1 + 2j
+
+# Print all examples
+print("Integer:", integer_example)
+print("Float:", float_example)
+print("String:", string_example)
+print("Boolean:", boolean_example)
+print("List:", list_example)
+print("Tuple:", tuple_example)
+print("Dictionary:", dictionary_example)
+print("Set:", set_example)
+print("NoneType:", none_example)
+print("Complex:", complex_example)
